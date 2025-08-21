f8216b74103d129e5d2b8b435f9f98ebba38e1c4
christal <christal.b.camillo@gmail.com>
2025-08-21 10:47:53 -0300

    usando função assincrona to improve. Error treatment

--- cli-cep/index.js
+++ cli-cep/index.js
@@ -6,21 +6,35 @@ https://viacep.com.br/ws/01001000/json/
 - Crie uma função que receba como argumento o cep e retorne um objeto com logradouro, cidade e estado. 
  */
 
-function retornaEndereco(cep) {
-    return fetch(`https://viacep.com.br/ws/${cep}/json/`)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            return {
-                logradouro: data.logradouro,
-                cidade: data.localidade,
-                estado: data.uf,
-            };
-        })
+async function retornaEndereco(cep) {
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+        if (!response.ok) {
+            throw new Error(`erro Http ${response.status}`)
+        }
+
+        const data = await response.json();
+
+        if (data.Error) {
+            throw new Error('cep não encontrado')
+        }
+
+        return {
+            logradouro: data.logradouro,
+            cidade: data.localidade,
+            estado: data.uf,
+
+        };
+    } catch (error) {
+        console.error('erro ao buscar cep', error);
+        throw error;
+    }
 }
 
 // chamando a função
 
 retornaEndereco('29090370')
     .then(endereco => console.log(endereco));
+// isso tinha na versão do professor mas aqui tá dando erro, tá faltando try
+// .catch (erro => console.log(erro));
