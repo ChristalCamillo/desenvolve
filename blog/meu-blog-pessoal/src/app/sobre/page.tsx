099b3b66f6c1e3f5d3adfa75191a8404346f9edd
christal <christal.b.camillo@gmail.com>
2025-09-10 11:14:26 -0300

    criação de links para navegação entre as páginas

--- blog/meu-blog-pessoal/src/app/sobre/page.tsx
+++ blog/meu-blog-pessoal/src/app/sobre/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function SobrePage() { 
     return (
         <main>
             <h1>Sobre mim</h1>
             <p>Meu nome é João Vitor, tenho 20 anos e sou estudante de Análise e Desenvolvimento de Sistemas. Atualmente, estou no meu terceiro semestre e estou muito animado com o que o futuro reserva para mim na área de tecnologia.</p>
+        
+        <Link href="/">Voltar para a página inicial</Link>
         </main>
     )
 }
