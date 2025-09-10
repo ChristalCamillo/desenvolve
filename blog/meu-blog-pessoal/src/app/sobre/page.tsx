29c413e2706e44aaa5761103f6ba3619fa857d54
christal <christal.b.camillo@gmail.com>
2025-09-10 11:05:26 -0300

    inicio do projeto, criação da pagina sobremin

---
+++ blog/meu-blog-pessoal/src/app/sobre/page.tsx
@@ -0,0 +1,8 @@
+export default function SobrePage() { 
+    return (
+        <main>
+            <h1>Sobre mim</h1>
+            <p>Meu nome é João Vitor, tenho 20 anos e sou estudante de Análise e Desenvolvimento de Sistemas. Atualmente, estou no meu terceiro semestre e estou muito animado com o que o futuro reserva para mim na área de tecnologia.</p>
+        </main>
+    )
+}
