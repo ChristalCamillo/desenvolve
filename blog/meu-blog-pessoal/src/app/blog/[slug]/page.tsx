566492d95cef0cb5dd37ec4e85a96b2063771301
christal <christal.b.camillo@gmail.com>
2025-09-11 10:59:10 -0300

    colocando o roteamento dinamico para paginas individuais de cada post

---
+++ blog/meu-blog-pessoal/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { posts } from "@/data/posts";
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function PostPage({ params }: Props) {
+  //Recebe o 'slug' da URL através dos parâmetros
+  const slug = params.slug;
+
+  // Encontra o post correspondente ao slug
+  const post = posts.find((post) => post.slug === params.slug);
+
+  // Se o post não for encontrado, retorna uma página 404
+  if (!post) {
+    return notFound();
+  }
+
+  //Se encontra o post, exibe seu título e conteúdo
+  return (
+    <main>
+      <div>
+        <h1 className="text-blue-500 text-3xl font-bold mb-4">{post.title}</h1>
+        <p className="text-gray-200">{post.content}</p>
+      </div>
+    </main>
+  );
+}
